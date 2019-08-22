c56d7df4e95ff83272b74c9a314bf93b9e10e1f8
elenems <mickhaelyarish@gmail.com>
2019-08-22 15:07:25 +0300

    Update config.js
    Removed firebase config fields

--- jobo-back/functions/util/config.js
+++ jobo-back/functions/util/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  apiKey: "AIzaSyDP7C_B6zEuhH_prRNt0PABnpJ8MhOa8cU",
-  authDomain: "jobo-745da.firebaseapp.com",
-  databaseURL: "https://jobo-745da.firebaseio.com",
-  projectId: "jobo-745da",
-  storageBucket: "jobo-745da.appspot.com",
-  messagingSenderId: "960618947643",
-  appId: "1:960618947643:web:704149022eecaa58"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "j",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
